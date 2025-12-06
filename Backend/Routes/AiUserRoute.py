6b26357ca1e241400980cce0587573be61f04f03
Eric Sweetman <esweetman2@gmail.com>
2025-12-06 13:17:18 -0500

    UpdateDate endpoint url

--- Backend/Routes/AiUserRoute.py
+++ Backend/Routes/AiUserRoute.py
@@ -15,7 +15,7 @@ router = APIRouter()
 #     return [{"username": "Rick"}, {"username": "Morty"}]
 
 
-@router.get("/users/", tags=["Users"])
+@router.get("/users", tags=["Users"])
 async def get_user(id: Optional[int] = None, session: Session = Depends(get_session)):
     try:
         _AiUserDatabase = AiUserDatabase(session)
@@ -27,7 +27,7 @@ async def get_user(id: Optional[int] = None, session: Session = Depends(get_sess
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-@router.get("/login/", tags=["Login Users"])
+@router.get("/login", tags=["Login Users"])
 async def login_user(email: str = None, session: Session = Depends(get_session)):
     try:
         _AiUserDatabase = AiUserDatabase(session)
@@ -39,7 +39,7 @@ async def login_user(email: str = None, session: Session = Depends(get_session))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-@router.post("/users/", tags=["Users"])
+@router.post("/users", tags=["Users"])
 async def add_user(email: str, display_name: str, session: Session = Depends(get_session)):
     try:
         print(email, display_name)
