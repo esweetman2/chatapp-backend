96d88e655724f3f18c1e4aee8211d7b6a443a649
esweetman <esweetman2@gmail.com>
2025-12-11 19:33:58 -0500

    Added Memeory model.

--- Backend/Models/MemoryModel.py
+++ Backend/Models/MemoryModel.py
@@ -4,21 +4,23 @@ from sqlalchemy.dialects.postgresql import JSONB
 from pgvector.sqlalchemy import Vector
 from datetime import datetime, timezone
 
-# CREATE TABLE agentmemories (
-#   id BIGSERIAL PRIMARY KEY,
-#   chat_id INT NOT NULL REFERENCES chats(id) ON DELETE CASCADE,
-#   agent_id INT NOT NULL REFERENCES agents(id) ON DELETE CASCADE,
-#   payload JSONB,
-#   embedding vector(1536),
-#   created_at TIMESTAMPTZ DEFAULT now()
+# CREATE TABLE agentmemory (
+#     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+#     agent_id INTEGER REFERENCES agents(id),
+#     user_id INTEGER REFERENCES aiusers(id),
+#     memory_text TEXT NOT NULL,
+#     embedding VECTOR(1536),
+#     created_date TIMESTAMPTZ NOT NULL DEFAULT NOW()
 # );
 
 class AgentMemory(SQLModel, table=True):
     __tablename__ = "agentmemory"
-    id: int = Field(primary_key=True)
-    chat_id: int = Field(foreign_key="chats.id")
+    id: Optional[int] = Field(default=None, primary_key=True)
     agent_id: Optional[int] = Field(default=None, foreign_key="agents.id")
-    payload: Dict[str, Any] = Field(sa_column=Column(JSONB))
+    user_id: Optional[int] = Field(default=None, foreign_key="aiusers.id")
+    
+    # payload: Dict[str, Any] = Field(sa_column=Column(JSONB))
+    memory_text: str = Field(nullable=False)
     embedding: Optional[list[float]] = Field(
         sa_column=Column(Vector(1536))
     )
